be204b555ea5fb3128696b5ab2936194d7ee6011
Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
2021-05-12 19:54:23 +0800

    enclave tls debuginfo

--- enclave-tls/src/tls_wrappers/wolfssl-sgx/cleanup.c
+++ enclave-tls/src/tls_wrappers/wolfssl-sgx/cleanup.c
@@ -11,8 +11,13 @@ tls_wrapper_err_t wolfssl_sgx_cleanup(tls_wrapper_ctx_t *ctx)
 {
 	ETLS_DEBUG("called\n");
 
-	tls_wrapper_err_t err;
-	ecall_wolfssl_cleanup((sgx_enclave_id_t)ctx->enclave_id, &err, ctx);
+	tls_wrapper_err_t err = 0;
+	ETLS_DEBUG("enclave_id = %d\n", (int)ctx->enclave_id);
+	sgx_status_t sgxret = ecall_wolfssl_cleanup((sgx_enclave_id_t)ctx->enclave_id, &err, ctx);
+    if (sgxret != SGX_SUCCESS) {
+        ETLS_ERR("ecall_wolfssl_cleanup(), sgxret = %d\n", sgxret);
+        return -TLS_WRAPPER_ERR_UNKNOWN;
+    }
 
 	return err;
 }
